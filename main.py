a5f44584d85292dde80275229a51b398ab573f00
Marianna Jevahiryan <mjevahiryan@mail.ru>
2022-04-26 17:00:45 +0400

    str

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def __str__(self):
+        return f'Person name is {self.name} and surname is {self.surname} and gender is {self.gender}'
 
     def grad(self, lector, course, grade):
         if isinstance(lector, Lector) and course in self.courses and course in lector.courses:
@@ -23,11 +25,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
 class Lector(Mentor):
     def __init__(self,name,surname):
         self.name = name
         self.surname = surname
         self.courses = {}
+    def __set__(self):
+        return f'Lector name is {self.name} and surname is {self.surname} '
 class Rewiures(Mentor):
     def __init__(self,name,surname):
         self.name = name
@@ -41,8 +46,12 @@ class Rewiures(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+    def __set__(self):
+        return f'Rewiures name is {self.name} and surname is {self.surname} and gender is {self.gender}'
 
-
+s = Student('Some','Buddy','boy')
+m = Lector('Alex','Bouldwin')
+r = Rewiures('May','Klein')
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Java']
@@ -63,4 +72,28 @@ best_lector.courses = {'Java' : 11}
 
 print(best_student.grades)
 print(best_lector.courses)
+
+print(s.__str__())
+print(m.__str__())
+print(r.__str__())
+
+
+
+# class Person:
+#
+#     def __init__(self, person_name, person_age):
+#         self.name = person_name
+#         self.age = person_age
+#
+#     def __str__(self):
+#         return f'Person name is {self.name} and age is {self.age}'
+#
+#     def __repr__(self):
+#         return f'Person(name={self.name}, age={self.age})'
+#
+#
+# p = Person('Pankaj', 34)
+#
+# print(p.__str__())
+# print(p.__repr__())
 
